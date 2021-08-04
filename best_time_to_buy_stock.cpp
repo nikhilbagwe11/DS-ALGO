e0dec25a44db80643e769f8c4d864fbb71c5212c
nikhilbagwe11 <86409972+nikhilbagwe11@users.noreply.github.com>
2021-08-04 20:20:54 +0530

    Update best_time_to_buy_stock.cpp

--- best_time_to_buy_stock.cpp
+++ best_time_to_buy_stock.cpp
@@ -1,5 +1,9 @@
 // Best Time to Buy and Sell Stock  = https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
+// Brute force = Check for every pair
+// 2nd approach = fix pointer at 1st index i.e at arr[0]->i=0, and then find the MAX element from i+1 to n and then increment i++
+
+// 3rd approach
 class Solution {
 public:
     int maxProfit(vector<int>& prices) 
